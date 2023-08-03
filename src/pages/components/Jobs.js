d62a90aaaca716fa35be2ed3d9f52085794f62ed
dr-robotnic456 <ernesteshun456@gmail.com>
2023-08-03 18:29:58 +0000

    first commit

--- src/pages/components/Jobs.js
+++ src/pages/components/Jobs.js
@@ -5,14 +5,23 @@ const Jobs = ({ showFilter, triggerFilter, result, handleFilter, handleLocationC
   return (
     <div className='xsm:w-[80%] sm:w-4/5 md:w-[85%] w-[90%] relative'>
       <div className='flex flex-wrap justify-center gap-2 after:w-[250px] after:h-[228px]'>
-        {showFilter && <Filter 
-          triggerFilter={triggerFilter}  
-          handleFilter={handleFilter} 
-          handleLocationChange={handleLocationChange} 
+        {showFilter && <Filter
+          triggerFilter={triggerFilter}
+          handleFilter={handleFilter}
+          handleLocationChange={handleLocationChange}
           locationQuery={locationQuery}
         />}
 
-        {result}
+        {result && result.length === 0 ? (
+          <div className='font-bold text-3xl h-screen'>
+            No Data Found
+          </div>
+        ) : (
+          result
+        )
+        }
+
+
 
       </div>
       <div className=' flex justify-center mt-0'>
