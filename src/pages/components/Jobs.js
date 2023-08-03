3e00da2ea123eb20814098963326798a36f1a069
dr-robotnic456 <ernesteshun456@gmail.com>
2023-08-03 18:32:47 +0000

    first commit

--- src/pages/components/Jobs.js
+++ src/pages/components/Jobs.js
@@ -1,34 +1,29 @@
-import React from 'react'
-import Filter from './Filter'
+import React from 'react';
+import Filter from './Filter';
 
-const Jobs = ({ showFilter, triggerFilter, result, handleFilter, handleLocationChange, locationQuery }) => {
+const Jobs = ({ showFilter, triggerFilter, result }) => {
+  const isResultEmpty = result.length === 0;
+
   return (
-    <div className='xsm:w-[80%] sm:w-4/5 md:w-[85%] w-[90%] relative'>
-      <div className='flex flex-wrap justify-center gap-2 after:w-[250px] after:h-[228px]'>
-        {showFilter && <Filter
-          triggerFilter={triggerFilter}
-          handleFilter={handleFilter}
-          handleLocationChange={handleLocationChange}
-          locationQuery={locationQuery}
-        />}
+    <div className={`xsm:w-[80%] sm:w-4/5 md:w-[85%] w-[90%] relative ${isResultEmpty ? 'h-screen' : ''}`}>
+      <div className='flex flex-wrap justify-center gap-2 after:w-[250px] after:h-[228px]' onClick={triggerFilter}>
+        {showFilter && <Filter triggerFilter={triggerFilter} />}
 
-        {result && result.length === 0 ? (
-          <div className='font-bold text-3xl h-screen'>
-            No Data Found
-          </div>
-        ) : (
-          result
-        )
-        }
-
-
+        {result}
 
       </div>
-      <div className=' flex justify-center mt-0'>
-        <button className='absolute bottom-10 bg-[#5964E0] py-2 rounded-lg px-5 hover:bg-[#939BF4]'>Load</button>
-      </div>
+      {isResultEmpty && (
+        <div className='flex justify-center items-center h-full'>
+          <p className='text-gray-500'>No jobs found.</p>
+        </div>
+      )}
+      {!isResultEmpty && (
+        <div className='flex justify-center mt-0'>
+          <button className='absolute bottom-10 bg-[#5964E0] py-2 rounded-lg px-5 hover:bg-[#939BF4]'>Load</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Jobs
+export default Jobs;
